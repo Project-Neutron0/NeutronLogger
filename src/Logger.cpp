ebb99484a1f2278c3673fa6251bef6dd4245e546
A_W1534 <addiktedmontage@gmail.com>
2022-12-13 00:31:14 +0000

    SIGTERM will no longer raise an exception so thats cool

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -124,7 +124,7 @@ namespace Logger {
                         oss << "\n    ";
                     }
 
-                    oss << fill(std::to_string(t_lines[i]), 2, ' ') << FORE_GRAY << " | " << RESET << BOLD << x << RESET;
+                    oss << fill(std::to_string(t_lines[i]), 2, ' ') BOLD << RESET << FORE_GRAY << " | " << RESET << BOLD << x << RESET;
                     i++;
                 }
                 std::string s = oss.str();
