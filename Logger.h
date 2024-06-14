51acbca319d03a4fd816ef0afe4b02d060c49e93
Andrew Wiltshire <62200778+AW1534@users.noreply.github.com>
2024-06-14 22:44:12 +0100

    moved ansi.h into logger.h. Now a single header file

--- Logger.h
+++ Logger.h
@@ -17,9 +17,53 @@
 #include <sstream>
 #include <csignal>
 #include <fstream>
-#include "ansi.h"
 
+#pragma region ANSI codes
+#define FORE_BLACK "\e[30m"
+#define FORE_RED "\e[31m"
+#define FORE_GREEN "\e[32m"
+#define FORE_YELLOW "\e[33m"
+#define FORE_BLUE "\e[34m"
+#define FORE_MAGENTA "\e[35m"
+#define FORE_CYAN "\e[36m"
+#define FORE_WHITE "\e[37m"
+#define FORE_GRAY "\e[90m"
+#define FORE_BRIGHT_RED "\e[91m"
+#define FORE_BRIGHT_GREEN "\e[92m"
+#define FORE_BRIGHT_YELLOW "\e[93m"
+#define FORE_BRIGHT_BLUE "\e[94m"
+#define FORE_BRIGHT_MAGENTA "\e[95m"
+#define FORE_BRIGHT_CYAN "\e[96m"
+#define FORE_BRIGHT_WHITE "\e[97m"
 
+#define BACK_BLACK "\e[40m"
+#define BACK_RED "\e[41m"
+#define BACK_GREEN "\e[42m"
+#define BACK_YELLOW "\e[43m"
+#define BACK_BLUE "\e[44m"
+#define BACK_MAGENTA "\e[45m"
+#define BACK_CYAN "\e[46m"
+#define BACK_WHITE "\e[47m"
+#define BACK_GRAY "\e[100m"
+#define BACK_BRIGHT_RED "\e[101m"
+#define BACK_BRIGHT_GREEN "\e[102m"
+#define BACK_BRIGHT_YELLOW "\e[103m"
+#define BACK_BRIGHT_BLUE "\e[104m"
+#define BACK_BRIGHT_MAGENTA "\e[105m"
+#define BACK_BRIGHT_CYAN "\e[106m"
+#define BACK_BRIGHT_WHITE "\e[107m"
+
+#define RESET "\e[0m"
+#define BOLD "\e[1m"
+#define DIM "\e[2m"
+#define ITALIC "\e[3m"
+#define UNDERLINE "\e[4m"
+#define SLOW_BLINK "\e[5m"
+#define RAPID_BLINK "\e[6m"
+#define INVERT "\e[7m"
+#define CONCEAL "\e[8m"
+#define CROSSED_OUT "\e[9m"
+#pragma endregion
 
 #ifndef BOOST_STACKTRACE_USE_BACKTRACE
 #ifndef COMPILE_RELEASE
